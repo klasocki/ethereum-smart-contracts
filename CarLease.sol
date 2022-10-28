cbfee8c6e960a75c106c8b4316b08b9463db0aa4
Remix Workspace <>
2022-10-28 17:04:00 +0200

    add auth & pricing test

--- CarLease.sol
+++ CarLease.sol
@@ -7,5 +7,73 @@ pragma solidity ^0.8.0;
  * @dev Implements car leasing
  */
 contract CarLease {
-    
+
+    enum MileageCap { SMALL, MEDIUM, LARGE, UNLIMITED }
+    enum ContractDuration { ONE_MONTH, THREE_MONTHS, SIX_MONTHS, TWELVE_MONTHS }
+
+    address payable private owner;
+    mapping (address => bool) private employees;
+
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    // Only the owner of the SC can call the function
+    modifier onlyOwner() {
+        require( msg.sender == owner , "Only owner can call this.");
+        _;
+    }
+
+    // Only the owner of the SC or the admins cal call the function
+    modifier onlyAdmin() {
+        require( msg.sender == owner || employees[msg.sender], "Only admins can call this.");
+        _;
+    }
+
+    // Add the given address to the employees list
+    function addEmployee(address _employee) public onlyOwner {
+        employees[_employee] = true;
+    }
+
+    // Remove the given address to the employees list
+    function removeEmployee(address _employee) public onlyOwner {
+        delete employees[_employee];
+    }
+
+    /// @notice Calculate the monthly quota of a car lease
+    /// @dev WORK IN PROGRESS this is just an example, it's still missing the car NFT in the calculation
+    /// @param yearsOfExperience the years of possession of a dricing license
+    /// @param mileageCap the selected mileage limit
+    /// @param duration the duration of the contract
+    /// @return Monthly quota in ether (?)
+    function calculateMonthlyQuota(uint8 yearsOfExperience, MileageCap mileageCap, ContractDuration duration) public pure returns(uint) {
+        
+        require(yearsOfExperience < 100, "yearsOfExperience must be less than 100.");
+
+        uint mileageFactor = 1;
+
+        if (mileageCap == MileageCap.MEDIUM) {
+            mileageFactor = 2;
+        } else if (mileageCap == MileageCap.LARGE) {
+            mileageFactor = 3;
+        } else {if (mileageCap == MileageCap.UNLIMITED) 
+            mileageFactor = 5;
+        }
+
+        uint durationFactor = 5;
+
+        if (duration == ContractDuration.THREE_MONTHS) {
+            durationFactor = 3;
+        } else if (duration == ContractDuration.SIX_MONTHS) {
+            durationFactor = 2;
+        } else if (duration == ContractDuration.TWELVE_MONTHS) {
+            durationFactor = 1;
+        }
+
+        uint quota = durationFactor * mileageFactor * (100 - yearsOfExperience);
+
+        return quota;
+    }
+
 }
