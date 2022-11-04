6ea401d78eb41ee590cee684f8e64a11cac5288f
Remix Workspace <>
2022-11-04 15:30:10 +0100

    rm: ERC721Enumerable

--- Car.sol
+++ Car.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
@@ -19,7 +18,7 @@ library CarLibrary {
     }
 }
 
-contract Car is ERC721, ERC721Enumerable, Ownable {
+contract Car is ERC721, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
@@ -35,25 +34,7 @@ contract Car is ERC721, ERC721Enumerable, Ownable {
         carsData[tokenId] = CarLibrary.CarData(model, colour, yearOfMatriculation, originalValue, kms, address(0));
         return tokenId;
     }
-
-    // The following functions are overrides required by Solidity.
-
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
-        internal
-        override(ERC721, ERC721Enumerable)
-    {
-        super._beforeTokenTransfer(from, to, tokenId);
-    }
-
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721Enumerable)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
-    }
-
+    
     function getCarData(uint carId) public view returns(CarLibrary.CarData memory) {
         return carsData[carId];
     }
